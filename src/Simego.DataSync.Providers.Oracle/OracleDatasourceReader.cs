0bbd0c22f8f859abdb9fa873701656a013ea83eb
Sean Cleaver <sean@simego.com>
2022-08-04 12:48:21 +0100

    Initial commit

---
+++ src/Simego.DataSync.Providers.Oracle/OracleDatasourceReader.cs
@@ -0,0 +1,28 @@
+using Oracle.ManagedDataAccess.Client;
+using Simego.DataSync.Providers.Ado;
+using System;
+using System.Data.Common;
+using System.Globalization;
+using System.Reflection;
+
+namespace Simego.DataSync.Providers.Oracle
+{
+    [ProviderInfo(Name = "Oracle Managed Data Access (21.6.1)", Description = "Oracle .NET Framework Managed Data Access", Group = "SQL")]
+    public class OracleDatasourceReader : AdoDataSourceReader
+    {
+        private Lazy<AdoDbProviderFactory> MyFactory => new Lazy<AdoDbProviderFactory>(() => new AdoDbProviderFactory("Oracle.ManagedDataAccess.Client", GetFactory()));
+
+        protected override bool IsCustomAdoProvider() => true;
+
+        public override AdoDbProviderFactory GetProviderFactory(string providerInvariantName) => MyFactory.Value;
+
+        private static DbProviderFactory GetFactory()
+        {
+            var factoryType = typeof(OracleClientFactory);
+
+            return factoryType.InvokeMember(factoryType.Name,
+               BindingFlags.CreateInstance | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public,
+               null, null, null, CultureInfo.CurrentCulture) as DbProviderFactory;
+        }
+    }
+}
