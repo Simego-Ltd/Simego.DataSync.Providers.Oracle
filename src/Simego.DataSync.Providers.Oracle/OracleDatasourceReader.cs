32a7707b88e3f254dada4e8ffaa968145bf53bff
Sean Cleaver <sean@simego.com>
2023-02-20 16:56:51 +0000

    Update Oracle Connector for V6

--- src/Simego.DataSync.Providers.Oracle/OracleDatasourceReader.cs
+++ src/Simego.DataSync.Providers.Oracle/OracleDatasourceReader.cs
@@ -1,6 +1,7 @@
 using Oracle.ManagedDataAccess.Client;
 using Simego.DataSync.Providers.Ado;
 using System;
+using System.Data;
 using System.Data.Common;
 using System.Globalization;
 using System.Reflection;
@@ -23,6 +24,48 @@ namespace Simego.DataSync.Providers.Oracle
             return factoryType.InvokeMember(factoryType.Name,
                BindingFlags.CreateInstance | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public,
                null, null, null, CultureInfo.CurrentCulture) as DbProviderFactory;
+        }
+
+        protected override void FillRegistryView(RegistryFolderType rootFolder, RegistryFolder tables, RegistryFolder views)
+        {
+            var factory = GetProviderFactory(ProviderName);
+
+            using (var connection = factory.GetConnection(ConnectionString.GetConnectionString()))
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+
+                using (var command = factory.CreateCommand($"SELECT ALL_OBJECTS.OWNER, ALL_OBJECTS.OBJECT_NAME, USER_OBJECTS.OBJECT_TYPE FROM ALL_OBJECTS INNER JOIN USER_OBJECTS ON USER_OBJECTS.OBJECT_ID = ALL_OBJECTS.OBJECT_ID WHERE USER_OBJECTS.OBJECT_TYPE = 'TABLE' OR USER_OBJECTS.OBJECT_TYPE = 'VIEW'", connection))
+                {
+                    command.CommandTimeout = CommandTimeout;
+                    command.CommandType = CommandType.Text;
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var schema = reader.GetString(0);
+                            var table = reader.GetString(1);
+                            var type = reader.GetString(2);
+
+                            var isDbSchema = schema.Equals(connection.Database, StringComparison.OrdinalIgnoreCase);
+                            var name = isDbSchema ? $"{table}" : $"{schema}.{table}";
+                            var qualifiedName = isDbSchema ? $"{StartQuoteChar}{table}{EndQuoteChar}" : $"{StartQuoteChar}{schema}{EndQuoteChar}.{StartQuoteChar}{table}{EndQuoteChar}";
+
+                            if (string.Equals(type, "VIEW", StringComparison.OrdinalIgnoreCase))
+                            {
+                                views.AddFolderItem(qualifiedName, name);
+                            }
+                            else
+                            {
+                                tables.AddFolderItem(qualifiedName, name);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
