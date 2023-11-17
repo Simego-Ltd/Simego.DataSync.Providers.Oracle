d3dd9335348b28607301b24a3563fd65a9ca6db6
Sean Cleaver <sean@simego.com>
2023-11-17 13:54:48 +0000

    Update project to use nuget package references.

--- src/build.bat
+++ src/build.bat
@@ -2,10 +2,12 @@ set p=Simego.DataSync.Providers.Oracle
 dotnet build -c Release
 rmdir ..\dist\ /S /Q
 mkdir ..\dist\files\%p%
-xcopy ..\src\%p%\bin\Release\net7.0-windows\*.* ..\dist\files\%p%\*.* /y /s
+xcopy ..\src\%p%\bin\Release\net8.0-windows7.0\*.* ..\dist\files\%p%\*.* /y /s
 cd ..\dist\files\
 del .\%p%\Simego.DataSync.Core.dll
 del .\%p%\Simego.DataSync.Providers.Ado.dll
+del .\%p%\Simego.DataSync.Security.dll
+del .\%p%\Simego.DataSync.SqlSchemaNavigator.dll
 tar.exe -acf ..\%p%.zip *.*
 cd ..\..\src
 
