376e5bf8de8e61b6e3bacca2d7b2eea3db4c192c
Sean Cleaver <sean@simego.com>
2024-01-30 14:06:41 +0000

    Update for Oracle Driver 21.13.0

--- src/build.bat
+++ src/build.bat
@@ -1,5 +1,5 @@
 set p=Simego.DataSync.Providers.Oracle
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /t:Build /p:Configuration=Release /p:NoWarn=1591
+"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\msbuild.exe" /t:Build /p:Configuration=Release /p:NoWarn=1591
 rmdir ..\dist\ /S /Q
 mkdir ..\dist\files\%p%
 xcopy ..\src\%p%\bin\Release\net472\*.* ..\dist\files\%p%\*.* /y
